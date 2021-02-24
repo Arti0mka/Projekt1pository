a1e81e2724395903e52a5e850bf0affff9752664
Arti0mka <74427717+Arti0mka@users.noreply.github.com>
2021-02-24 23:41:43 +0300

    Update tele3.py

--- tele3.py
+++ tele3.py
@@ -5,14 +5,24 @@ import telebot
 import time
 #pip3 install pytelegrambotapi --upgrade
 
-bot = telebot.TeleBot('1455219084:AAHQJ345bCjyZNJXoTbbZhsjVGbr5tmTBns')
-
+bot = telebot.TeleBot('1287725214:AAEPpUk4MmCQOl5FDBs5IiF3zzNRQXHnB6Q')
+arin = 0
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
+    global arin
+    if str(message.from_user.id) == "1226994201":
+       arin = arin + 1
+    if str(message.from_user.id) == "1369930501":
+       arin = arin + 5
     print(message.text)
     if message.text == "/help":
-        bot.send_message(message.from_user.id, "i am stupit")
+        bot.send_message(message.from_user.id, "i am stupit")        
+    print(message.text)
+    if message.text == "Мими":
+        bot.send_message(message.from_user.id, "user.id")
+        print(message.text)
+        bot.send_message(message.from_user.id, str(message.from_user.id))
     else:
-        bot.send_message(message.from_user.id, "i undestend write /help.")
+        bot.send_message(message.from_user.id, arin)
 
-bot.polling(none_stop=True, interval=0)	
+bot.polling(none_stop=True, interval=0)
